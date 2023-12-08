ad98a402daa6d49bf2fa375c71b23993a156b95f
Kethepalli Venkata Lakshmana Praveen <190030921@kluniversity.in>
2023-12-08 11:42:03 +0530

    Update urls.py

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,17 +1,18 @@
 # ratings/urls.py
 from django.urls import path
 from . import views
+from .views import all_ratings, DeleteRatingView
 
 
 
 urlpatterns = [
     #path('', views.home, name='home'),
     path('brewery_list/', views.brewery_list, name='brewery_list'),  # Add this line for the root path
-    path('<str:brewery_phone>/brewery_detail/', views.brewery_detail, name='brewery_detail'),
-    path('<str:brewery_phone>/add_rating/', views.add_rating, name='add_rating'),
-    path('<str:brewery_phone>/delete_rating/', views.delete_rating, name='delete_rating'),
+    path('brewery_search/', views.brewery_search, name='brewery_search'),
+    path('brewery_detail/<uuid:brewery_id>/', views.brewery_detail, name='brewery_detail'),
+    path('add_rating/<uuid:brewery_id>/', views.add_rating, name='add_rating'),
+    path('delete_rating/<int:rating_id>/', DeleteRatingView.as_view(), name='delete_rating'),
     path('all_ratings/', views.all_ratings, name='all_ratings'),  # Add this line for all ratings
-    path('ratings/', views.ratings, name='ratings'),
     path('', views.LoginPage, name='login'),
     path('signup/', views.SignupPage, name='signup'),  # Update this line to use the correct signup view
     path('profile/', views.profile, name='profile'),
